8ff79537fa727cce2f51b3c627c97fb6014f498a
Jameel Syed <jasyed@acm.org>
2011-09-25 22:30:51 +0100

    Initial upload

---
+++ boilerplate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+
+def main(argv):
+    """
+    Do something useful
+    """
+
+    print "Hello, World"
+
+    return 0
+ 
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
